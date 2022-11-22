fb75c08564b50e3bdde0f72714862be26c8db154
Вадим <54228342+VadimFalaleev@users.noreply.github.com>
2022-11-22 15:12:16 +0500

    CloneRepo

---
+++ Game Service Dev LAB5/Assets/_Scripts/DragonPicker.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DragonPicker : MonoBehaviour
+{
+    public GameObject energyShieldPrefab;
+    public int numEnergyShield = 3;
+    public float energyShieldBottomY = -6;
+    public float energyShieldRadius = 1.5f;
+    public List<GameObject> shieldList;
+
+    private void Start()
+    {
+        shieldList = new();
+
+        for (int i = 1; i <= numEnergyShield; i++)
+        {
+            GameObject tShieldGo = Instantiate(energyShieldPrefab);
+            tShieldGo.transform.position = new(0, energyShieldBottomY, 0);
+            tShieldGo.transform.localScale = new(1 * i, 1 * i, 1 * i);
+            shieldList.Add(tShieldGo);
+        }
+    }
+
+    public void DragonEggDestroyed()
+    {
+        GameObject[] tDragonEggArray = GameObject.FindGameObjectsWithTag("Dragon Egg");
+        foreach (GameObject tGO in tDragonEggArray)
+            Destroy(tGO);
+
+        int shieldIndex = shieldList.Count - 1;
+        GameObject tShieldGo = shieldList[shieldIndex];
+        shieldList.RemoveAt(shieldIndex);
+        Destroy(tShieldGo);
+
+        if (shieldList.Count == 0)
+            SceneManager.LoadScene("_0Scene");
+    }
+}
