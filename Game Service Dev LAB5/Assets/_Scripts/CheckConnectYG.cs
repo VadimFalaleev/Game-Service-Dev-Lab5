773fdcbce9c8e618b99e9decc3d3e1218f7aa42c
Вадим <54228342+VadimFalaleev@users.noreply.github.com>
2022-11-22 18:49:42 +0500

    Video1

---
+++ Game Service Dev LAB5/Assets/_Scripts/CheckConnectYG.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using YG;
+
+public class CheckConnectYG : MonoBehaviour
+{
+    private void OnEnable() => YandexGame.GetDataEvent += CheckSDK;
+    private void OnDisable() => YandexGame.GetDataEvent -= CheckSDK;
+    void Start()
+    {
+        if (YandexGame.SDKEnabled)
+        {
+            CheckSDK();
+        }
+    }
+
+    public void CheckSDK()
+    {
+        if (YandexGame.auth)
+        {
+            Debug.Log("User authorization ok");
+        }
+        else
+        {
+            Debug.Log("User not authorization");
+            YandexGame.AuthDialog();
+        }
+    }
+}
